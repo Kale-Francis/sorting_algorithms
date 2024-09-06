5c4efd39f1347ba3a2f3e903349dcfffd2cbe44e
king-vader <kalefrancis989@gmail.com>
2024-09-06 16:48:30 +0300

    updated

--- print_list.c
+++ print_list.c
@@ -2,21 +2,21 @@
 #include "sort.h"
 
 /**
- * print_list - Prints a list of integers.
- * @list: The list to be printed.
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
  */
 void print_list(const listint_t *list)
 {
-    int i;
+    int i = 0;
 
-    i = 0;
     while (list)
     {
         if (i > 0)
             printf(", ");
         printf("%d", list->n);
-        ++i;
         list = list->next;
+        i++;
     }
     printf("\n");
 }
